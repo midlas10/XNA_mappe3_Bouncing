8786a7d601579a131f5c4b2f73ad104041c7f10e
Nicolas N <nornic10@nith.no>
2012-10-09 15:14:52 +0200

    Commit

---
+++ Bouncing/Bouncing/Sprite.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Bouncing
+{
+    class Sprite
+    {
+        Texture2D image;
+
+        protected Point imageSize;
+        protected Point frameSize;
+        protected Point currentFrame;
+        protected Vector2 position, speed;
+
+
+        public Sprite()
+        {
+
+        }
+
+        public abstract Vector2 direction
+        {
+            get;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+        }
+
+    }
+}
