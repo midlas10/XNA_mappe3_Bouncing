45edd990cc6c6346f533df2f4d72b5b09534d564
Nicolas N <nornic10@nith.no>
2012-10-19 09:58:59 +0200

    Ah, så det e sånn det e.

--- Bouncing/Bouncing/GameObjects/Enemy.cs
+++ Bouncing/Bouncing/GameObjects/Enemy.cs
@@ -6,16 +6,20 @@ namespace Bouncing.GameObjects
 {
     public class Enemy : CollisionSystem.GameObjectCollidable
     {
-        protected Texture2D art;
+        protected Texture2D image;
         protected Game game;
-
-        protected float rotation;
         protected SpriteBatch spriteBatch;
+        protected ObjectManager objectManager;
+
+        Point frameSize = new Point(300, 300);
+        Point currentFrame = new Point(0, 0);
+        Point imageSize = new Point(3, 3);
+
+        int timeSinceLastFrame = 0;
+        int msPerFrame = 200;
 
 
-        protected ObjectManager om;
-
-        public Enemy(Game baseGame, Vector2 position, SpriteBatch spriteBatchToUse)
+        public Enemy(Game baseGame, SpriteBatch spriteBatchToUse, Vector2 position)
             : base(position)
         {
             spriteBatch = spriteBatchToUse;
@@ -23,15 +27,50 @@ namespace Bouncing.GameObjects
             collisionBox = new Rectangle((int) position.X, (int) position.Y, 0, 0);
         }
 
+        public void LoadContent()
+        {
+            image = game.Content.Load<Texture2D>(@"Images/Enemies/vortex");
+            collisionBox.Width = image.Width;
+            collisionBox.Height = image.Height;
+            objectManager = (ObjectManager)game.Services.GetService(typeof(ObjectManager));
+        }
+
         public override void Update(GameTime gameTime)
         {
+            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (timeSinceLastFrame > msPerFrame)
+            {
+                timeSinceLastFrame -= msPerFrame;
+
+                ++currentFrame.X;
+                if (currentFrame.X >= imageSize.X)
+                {
+                    currentFrame.X = 0;
+                    ++currentFrame.Y;
+                    if (currentFrame.Y >= imageSize.Y)
+                        currentFrame.Y = 0;
+                }
+            }
+            
+
             
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
-            
+            spriteBatch.Draw(image, 
+                Vector2.Zero,
+                new Rectangle(currentFrame.X * frameSize.X,
+                    currentFrame.Y * frameSize.Y,
+                    frameSize.X,
+                    frameSize.Y),
+                    Color.White,
+                    0,
+                    Vector2.Zero,
+                    1,
+                    SpriteEffects.None,
+                    0);
         }
 
     }
--- Bouncing/Bouncing/GameObjects/Player.cs
+++ Bouncing/Bouncing/GameObjects/Player.cs
@@ -13,7 +13,7 @@ namespace Bouncing.GameObjects
         protected SpriteBatch spriteBatch;
         protected Game game;
 
-        protected Texture2D playerArt;
+        protected Texture2D image;
         protected Rectangle playerAreal;
 
         protected float rotation;
@@ -24,6 +24,10 @@ namespace Bouncing.GameObjects
         protected ObjectManager objectManager;
         protected IManageCollisionsService collisionManager;
         protected IInputService _input;
+
+        Point frameSize = new Point(300, 300);
+        Point currentFrame = new Point(0, 0);
+        Point imageSize = new Point(3, 3);
 
         public Player(Game baseGame, SpriteBatch spriteBatchToUse, Vector2 position)
             : base(position)
@@ -37,9 +41,15 @@ namespace Bouncing.GameObjects
 
         public void LoadContent()
         {
+<<<<<<< HEAD
             playerArt = game.Content.Load<Texture2D>(@"Images/Player/blob");
             collisionBox.Width = 20;
             collisionBox.Height = 20;
+=======
+            image = game.Content.Load<Texture2D>(@"Images/Player/blob");
+            collisionBox.Width = image.Width;
+            collisionBox.Height = image.Height;
+>>>>>>> MenuScreens etc.
             objectManager = (ObjectManager) game.Services.GetService(typeof (ObjectManager));
             collisionManager = (IManageCollisionsService) game.Services.GetService((typeof (IManageCollisionsService)));
             _input = (IInputService) game.Services.GetService(typeof (IInputService));
@@ -47,36 +57,72 @@ namespace Bouncing.GameObjects
 
         public override void Update(GameTime gameTime)
         {
+            currentFrame.X = 1;
+            currentFrame.Y = 1;
+
             if(_input.IsKeyDown(Keys.W) || _input.IsKeyDown(Keys.Up))
             {
                 ChangeAnimation(7);
                 position.Y -= movementPerSecond*(float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                currentFrame.X = 1;
+                currentFrame.Y = 0;
             } 
             else if(_input.IsKeyDown(Keys.S) || _input.IsKeyDown(Keys.Down))
             {
                 ChangeAnimation(1);
                 position.Y += movementPerSecond*(float) gameTime.ElapsedGameTime.TotalSeconds;
+
+                currentFrame.X = 1;
+                currentFrame.Y = 2;
             }
 
-            if(_input.IsKeyDown(Keys.A) || _input.IsKeyDown(Keys.Left))
+            else if(_input.IsKeyDown(Keys.A) || _input.IsKeyDown(Keys.Left))
             {
                 ChangeAnimation(5);
                 position.X -= movementPerSecond * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                
+                currentFrame.X = 0;
+                currentFrame.Y = 1;
             }
             else if (_input.IsKeyDown(Keys.D) || _input.IsKeyDown(Keys.Right))
             {
                 ChangeAnimation(3);
                 position.X += movementPerSecond * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                currentFrame.X = 2;
+                currentFrame.Y = 1;
             }
+<<<<<<< HEAD
             rotation += 40 * (float)gameTime.ElapsedGameTime.TotalSeconds;
             collisionBox = new Rectangle((int)position.X, (int)position.Y, SpriteWidth, SpriteHeight);
+=======
+
+            collisionBox = new Rectangle((int)position.X, (int)position.Y, image.Width, image.Height);
+            
+>>>>>>> MenuScreens etc.
             
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
+<<<<<<< HEAD
             spriteBatch.Draw(playerArt, collisionBox, sourceRectangle, Color.White, 0f, new Vector2(playerArt.Width / 3, playerArt.Height / 3), SpriteEffects.None, 1f);
+=======
+            spriteBatch.Draw(image, 
+                collisionBox, 
+                new Rectangle(currentFrame.X * frameSize.X,
+                    currentFrame.Y * frameSize.Y,
+                    frameSize.X,
+                    frameSize.Y), 
+                    Color.White, 
+                    0f, 
+                    Vector2.Zero, 
+                    SpriteEffects.None, 
+                    0);
+
+>>>>>>> MenuScreens etc.
             base.Draw(gameTime);
         }
 
