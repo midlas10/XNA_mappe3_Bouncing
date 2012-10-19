e213432d182819ea14af53f01ed1dafd8aa3b63e
Sebastian Sebusæter <sebastian.sebusaeter@gmail.com>
2012-10-19 11:17:56 +0200

    Begynnelse på star clase

---
+++ Bouncing/Bouncing/GameObjects/Star.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bouncing.CollisionSystem;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Bouncing.GameObjects
+{
+    public class Star : GameObjectCollidable
+    {
+        protected SpriteBatch spriteBatch;
+        protected Game game;
+
+        public Star(Game baseGame, Vector2 position, SpriteBatch spriteBatchToUse)
+            : base(position)
+        {
+            spriteBatch = spriteBatchToUse;
+            game = baseGame;
+            collisionBox = new Rectangle((int) position.X, (int) position.Y, 0, 0);
+        }
+    }
+}
